12c073a5f307e64a1768c1c5419ac9b8ebe90152
romansalakhov <salakhov.roman12@gmail.com>
2019-12-02 12:22:51 +0500

    fixed merge conflict

--- Model/Program.cs
+++ Model/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Model
+﻿using System.Collections.Generic;
+
+namespace Model
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var list = new List<Job>();
+            list.Add(new Job(6, 5));
+            list.Add(new Job(4, 2));
+            list.Add(new Job(6, 3));
+            list.Add(new Job(5, 6));
+            list.Add(new Job(7, 6));
+            list.Add(new Job(1, 1));
+            list.Add(new Job(4, 7));
             
+            list.Sort(new JobComparer());
         }
     }
 }
