2d5d46f32cee34663a8a5dfa181a606818716b93
romansalakhov <salakhov.roman12@gmail.com>
2019-12-02 12:02:54 +0500

    added class "Job"

---
+++ Model/Job.cs
@@ -0,0 +1,30 @@
+namespace Model
+{
+    public class Job
+    {
+        private static char namer = 'A';
+        
+        public int FirstStage { get; set; }
+        
+        public int SecondStage { get; set; }
+        
+        public string Name { get; }
+
+        public Job(int fStage, int secStage)
+        {
+            FirstStage = fStage;
+            SecondStage = secStage;
+            Name = namer.ToString();
+            if (namer == 'Z')
+            {
+                namer = 'A';
+            }
+            else
+            {
+                namer++;
+            }
+        }
+        
+        
+    }
+}
