86c787523e0741645aedc74b39857791a8a26876
romansalakhov <salakhov.roman12@gmail.com>
2019-12-02 23:46:03 +0500

    updated class "JobComparer"

--- Model/JobComaparer.cs
+++ Model/JobComaparer.cs
@@ -7,6 +7,8 @@ namespace Model
     {
         public int Compare(Job x, Job y)
         {
+            if (x.FirstStage == x.SecondStage)
+                return -1;
             return Math.Min(x.FirstStage, y.SecondStage).CompareTo(Math.Min(x.SecondStage, y.FirstStage));
         }
     }
